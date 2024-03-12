0c93b477f10ae4f8d54224f705c0848f724225ac
Vishwas Mishra <134942427+Vishwaash@users.noreply.github.com>
2024-03-13 00:11:56 +0530

    project4

--- projects/Guess the number/chaiaurcode.js
+++ projects/Guess the number/chaiaurcode.js
@@ -12,8 +12,29 @@ let numGuess = 1
 
 let playGame = true
 
+if(playGame){
+    submit.addEventListener('click',function(e){
+        e.preventDefault()
+        const guess = parseInt(userInput.value)
+        console.log(guess);
+        validateGuess(guess)
+    })
+}
 function validateGuess(guess){
-    //
+    if(isNaN(guess)){
+        alert('Please enter a valid number')
+    } else if(guess<1){
+        alert('please enter a valid number')
+    } else if(guess>100){
+        alert('please enter a number less than 100')
+    }
+    else{
+        prevGuess.push(guess)
+        if(numGuess === 11){
+            displayguess(guess)
+            displayMessage(`Game Over. Random Number was ${randomnumber}`)
+        }
+    }
 }
 function checkguess(guess){
 
