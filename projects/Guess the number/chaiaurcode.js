18c3c9e049e3166db2dc061f0c88fdfda3557c49
Vishwas Mishra <134942427+Vishwaash@users.noreply.github.com>
2024-03-12 22:04:39 +0530

    guessthenumber

---
+++ projects/Guess the number/chaiaurcode.js
@@ -0,0 +1,33 @@
+const randomnumber = console.log(parseInt(Math.random()* 100 + 1));
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessslot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+
+function validateGuess(guess){
+    //
+}
+function checkguess(guess){
+
+}
+
+function displayguess(){
+    //
+}
+function displayMessage(message){
+    //
+}
+function newGame(){
+    //
+}
+function endGame(){
+    //
+}
